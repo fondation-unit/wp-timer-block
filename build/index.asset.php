8193b0b8774625da244a051bb128528488c4d52e
Hipjea <pierre.duverneix@gmail.com>
2024-03-28 16:33:33 +0100

    fix setTimerDuration

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ecd7b3c462b201b27479');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '398c662f419b7388eedd');
--- timer-block.php
+++ timer-block.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Timer Block
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       A client side timer within WordPress pages.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Pierre Duverneix - Fondation UNIT
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       timer-block

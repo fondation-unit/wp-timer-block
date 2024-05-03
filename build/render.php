34a2216e606ad99086b891a4ffcf1cc4526350b9
Hipjea <pierre.duverneix@gmail.com>
2024-05-03 10:13:27 +0200

    icons

--- build/render.php
+++ build/render.php
@@ -17,8 +17,16 @@ if ( !empty( $attributes['timerDuration'] ) ) : ?>
         </div>
       </div>
 
-      <button class="btn btn-primary btn-start" type="button">Start</button>
-      <button class="btn btn-primary btn-pause d-none" type="button">Pause</button>
+      <button class="btn btn-primary btn-start" type="button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+          <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
+        </svg>
+      </button>
+      <button class="btn btn-primary btn-pause d-none" type="button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5"/>
+        </svg>
+      </button>
     </div>
   </div>
 
